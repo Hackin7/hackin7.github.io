55e8b8863db9cd976f1d6307e3394bede62c42f3
name <zunmun@gmail.com>
2023-02-18 17:39:05 +0800

    feat(v1): github pages integration and sveltekit

---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static"
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/page" : "",
+    },
+	}
+};
+
+export default config;
